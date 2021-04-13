9cc2d3cf1e4d2e1b68db3662b683317ee6e0770c
Jonathan Hatton <56257755+JEGHatton@users.noreply.github.com>
2021-04-13 16:38:08 +0100

    Update BlogController.php
    Added the editComment function, deleteComment function and updating the getBlogs function.

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
+    public function getBlogs()
+    {
+        $blogs = Blogs::published()->orderByDesc('published_at')->simplePaginate(10);
 
+        return view('/', compact($blogs));
+    }
+
+    public function getBlogsAndComments()
+    {
+        return view('/');
+    }
+
+    public function createComment(Request $request){
+        $comment = Comment::create(array(
+            'title' => Input::get('title'),
+            'comment' => Input::get('comment'),
+            'name' => Input::get('name')
+        ));
+        return redirect()->route('home')->with('success', 'Comment has been successfully added!');
+    }
+
+    public function editComment($id) {
+        $comment = Comment::find($id);
+        return view('Blog/edit_comment', ['comment' => $comment]);
+    }
+
+    public function deleteComment()
+    {
+        return view('/');
+    }
 }
